39f34274e4f132140b022f2a2077730ec3690067
DeveloperFsc <74286187+DeveloperFsc@users.noreply.github.com>
2020-11-11 15:51:53 +0530

    Add files via upload

---
+++ templates/home.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Template Name: Homepage
+ *
+ * Template Post Type: post, page
+ *
+ * The template for displaying Page/Post with No Sidebar, Full Width
+ *
+ * @package JetBlack
+ */
+
+get_header();
+?>
+<!-- <div class="container">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main"> -->
+
+    <?php
+    while ( have_posts() ) :
+        the_post();
+
+        get_template_part( 'template-parts/content/content', get_post_type() );
+
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
+
+    endwhile; // End of the loop.
+    ?>
+
+    <!-- </main>
+</div>
+</div> -->
+<?php
+get_footer();
